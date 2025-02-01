57aea1fc49851ae98af7dae371ff7e2a69adae48
Pedro-Roger <eng.pedrodev@gmail.com>
2025-01-31 22:48:31 -0300

    module user & controller added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
---
+++ src/user/user.controller.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+
+
+@Controller('user')
+export class UserController {
+    @Get()
+    async  getAllUsers() {
+       return JSON.stringify({test: 'abc'});
+    }
+}
